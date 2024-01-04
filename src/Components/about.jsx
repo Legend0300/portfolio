56c3d0ecdfea411dc97a87c502318f3f507a490c
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2024-01-04 18:06:33 +0500

    added more styling and CV

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -1,19 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { Fade, Slide } from 'react-reveal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLaptopCode, faRobot, faBrain } from '@fortawesome/free-solid-svg-icons';
-import { faReact, faJs, faPython, faNodeJs } from '@fortawesome/free-brands-svg-icons';
+import { faLaptopCode, faRobot, faBrain, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faReact, faJsSquare, faPython, faNodeJs, faJs } from '@fortawesome/free-brands-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(faLaptopCode, faRobot, faBrain, faChevronLeft, faChevronRight, faReact, faJsSquare, faPython, faNodeJs, faJs);
 
 const About = () => {
   const [currentCard, setCurrentCard] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setCurrentCard((prevCard) => (prevCard + 1) % 3);
-    }, 6000); // Change the timeout to 6 seconds
+      setCurrentCard((prevCard) => (prevCard + 1) % 4);
+    }, 6000);
 
     return () => clearTimeout(timer);
   }, [currentCard]);
+
+  const shiftLeft = () => setCurrentCard((prevCard) => (prevCard + 3) % 4);
+  const shiftRight = () => setCurrentCard((prevCard) => (prevCard + 1) % 4);
+
+  const dotStyle = (index) => ({
+    width: '8px',
+    height: '8px',
+    backgroundColor: index === currentCard ? '#00BFA6' : '#718096',
+    borderRadius: '50%',
+    margin: '0 4px',
+    transition: 'background-color 0.3s ease-in-out',
+  });
 
   const cards = [
     {
@@ -57,19 +85,19 @@ const About = () => {
       ),
     },
     {
-        title: 'MERN Stack and Node.js',
-        icon: faNodeJs,
-        content: (
-          <>
-            <p className="mb-3">
-              Proficient in the MERN (MongoDB, Express.js, React, Node.js) stack for building full-stack web applications.
-            </p>
-            <p>
-              Additionally, experienced in server-side JavaScript programming with Node.js, enabling the development of scalable and efficient server applications.
-            </p>
-          </>
-        ),
-      },
+      title: 'MERN Stack and Node.js',
+      icon: faNodeJs,
+      content: (
+        <>
+          <p className="mb-3">
+            Proficient in the MERN (MongoDB, Express.js, React, Node.js) stack for building full-stack web applications.
+          </p>
+          <p>
+            Additionally, experienced in server-side JavaScript programming with Node.js, enabling the development of scalable and efficient server applications.
+          </p>
+        </>
+      ),
+    },
     {
       title: 'Generative AI and Trending Technologies',
       icon: faRobot,
@@ -83,23 +111,48 @@ const About = () => {
         </>
       ),
     },
-    ];
-  
-    return (
-      <div className="about-container bg-gray-900 py-12 text-white h-full flex justify-center items-center">
-        <Slide left>
-          <Fade duration={1000}>
-            <div className="card text-lg p-6 rounded bg-gray-800 max-w-2xl w-full">
-              <h3 className="text-2xl font-bold mb-3 text-teal-400 flex items-center">
-                <FontAwesomeIcon icon={cards[currentCard].icon} className="mr-2" />
+  ];
+
+  return (
+    <div className="about-container bg-gray-900 py-8 md:py-12 text-white h-full flex flex-col items-center">
+      <Slide left>
+        <Fade duration={1000}>
+          <div className="card text-lg p-4 md:p-6 rounded bg-gray-800 max-w-2xl w-full md:flex items-center">
+            {windowWidth >= 768 && (
+              <button
+                style={{ padding: "5px", marginRight: "5px" }}
+                onClick={shiftLeft}
+                className="text-white p-2 rounded-full focus:outline-none hover:transform hover:scale-105 transition-transform"
+              >
+                <FontAwesomeIcon icon="chevron-left" className="text-lg" />
+              </button>
+            )}
+            <div style={{ paddingRight: "10px" }} className="px-4 md:px-6 flex-grow">
+              <h3 className="text-xl md:text-2xl font-bold mb-3 text-teal-400 md:mb-0 flex items-center">
+                <FontAwesomeIcon icon={faLaptopCode} className="mr-2" />
                 {cards[currentCard].title}
               </h3>
               {cards[currentCard].content}
             </div>
-          </Fade>
-        </Slide>
+            {windowWidth >= 768 && (
+              <button
+                style={{ padding: "5px", marginLeft: "5px", marginBottom: "1%" }}
+                onClick={shiftRight}
+                className="text-white p-2 rounded-full focus:outline-none hover:transform hover:scale-105 transition-transform"
+              >
+                <FontAwesomeIcon icon="chevron-right" className="text-lg" />
+              </button>
+            )}
+          </div>
+        </Fade>
+      </Slide>
+      <div className="flex items-center mt-2 md:mt-4">
+        {[0, 1, 2, 3].map((index) => (
+          <div key={index} style={dotStyle(index)} />
+        ))}
       </div>
-    );
-  };
-  
-  export default About;
+    </div>
+  );
+};
+
+export default About;
