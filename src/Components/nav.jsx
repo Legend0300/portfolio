af604080830aaa570f5edd677f3d00f188327355
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2024-01-04 18:14:31 +0500

    updated nav

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -3,130 +3,135 @@ import { NavLink, Outlet } from 'react-router-dom';
 import Footer from './footer';
 
 const Nav = () => {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  const toggleMobileMenu = () => {
-    setMobileMenuOpen(!isMobileMenuOpen);
-  };
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
 
-  return (
-    <div className="bg-gray-900 text-white p-4 transition-colors duration-300">
-      <nav className="flex justify-between items-center">
-        <div className="hidden lg:flex space-x-4">
-          {/* Styled Logo */}
-          <div className="flex items-center">
-            <NavLink
-              to="/"
-              end
-              className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
-              exact
-            >
-              Home
-            </NavLink>
-          </div>
-          <NavLink
-            to="skills"
-            className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
-          >
-            Skills
-          </NavLink>
-          <NavLink
-            to="projects"
-            className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
-          >
-            Projects
-          </NavLink>
-          <NavLink
-            to="certifications"
-            className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
-          >
-            Certifications
-          </NavLink>
-          <NavLink
-            to="contact"
-            className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
-          >
-            Contact
-          </NavLink>
-                {/* Download CV Button */}
-      <a
-        href="src\CV (1).PDF"
-        download="https://c5y18s9mdhikovk1.public.blob.vercel-storage.com/CV%20(1).PDF"
-        className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
-      >
-        Download CV
-      </a>
+    return (
+        <div className="bg-gray-900 text-white p-4 transition-colors duration-300">
+            <nav className="flex justify-between items-center">
+                <div className="hidden lg:flex space-x-4">
+                    {/* Styled Logo */}
+                    <div className="flex items-center">
+                        <NavLink
+                            to="/"
+                            end
+                            className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
+                            exact
+                        >
+                            Home
+                        </NavLink>
+                    </div>
+                    <NavLink
+                        to="skills"
+                        className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
+                    >
+                        Skills
+                    </NavLink>
+                    <NavLink
+                        to="projects"
+                        className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
+                    >
+                        Projects
+                    </NavLink>
+                    <NavLink
+                        to="certifications"
+                        className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
+                    >
+                        Certifications
+                    </NavLink>
+                    <NavLink
+                        to="contact"
+                        className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
+                    >
+                        Contact
+                    </NavLink>
+                    {/* Download CV Button */}
+                    <a
+                        href="https://c5y18s9mdhikovk1.public.blob.vercel-storage.com/CV%20(1).PDF"
+                        download="cv (1).pdf"
+                        className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
+                        target="_blank"  // Open link in a new tab/window
+                        rel="noopener noreferrer"  // Recommended for security when using target="_blank"
+                    >
+                        Download CV
+                    </a>
+
+                </div>
+                {/* Mobile Menu Toggle Button */}
+                <button
+                    onClick={toggleMobileMenu}
+                    className="lg:hidden flex items-center text-white focus:outline-none transition duration-300 ease-in-out"
+                >
+                    <div className="w-6 h-6">
+                        <div className="bg-white h-1 w-full mb-1 rounded-full"></div>
+                        <div className="bg-white h-1 w-full mb-1 rounded-full"></div>
+                        <div className="bg-white h-1 w-full rounded-full"></div>
+                    </div>
+                </button>
+            </nav>
+
+            {isMobileMenuOpen && (
+                <div className="lg:hidden mt-2 transition-opacity duration-300 animate-fadeIn">
+                    {/* Styled Logo */}
+                    <div className="flex items-center justify-center mb-4">
+                        {/* Your logo or text here */}
+                    </div>
+                    <NavLink
+                        to="/"
+                        onClick={toggleMobileMenu}
+                        className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
+                        exact
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink
+                        to="skills"
+                        onClick={toggleMobileMenu}
+                        className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
+                    >
+                        Skills
+                    </NavLink>
+                    <NavLink
+                        to="projects"
+                        onClick={toggleMobileMenu}
+                        className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
+                    >
+                        Projects
+                    </NavLink>
+                    <NavLink
+                        to="certifications"
+                        onClick={toggleMobileMenu}
+                        className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
+                    >
+                        Certifications
+                    </NavLink>
+                    <NavLink
+                        to="contact"
+                        onClick={toggleMobileMenu}
+                        className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
+                    >
+                        Contact
+                    </NavLink>
+                    {/* Download CV Button for Mobile */}
+                    <a
+                        href="https://c5y18s9mdhikovk1.public.blob.vercel-storage.com/CV%20(1).PDF"
+                        download="cv (1).pdf"
+                        className="font-medium nav-link text-teal-400 hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out rounded-full p-3"
+                        target="_blank"  // Open link in a new tab/window
+                        rel="noopener noreferrer"  // Recommended for security when using target="_blank"
+                    >
+                        Download CV
+                    </a>
+                </div>
+            )}
+
+            <Outlet />
+            <Footer />
         </div>
-        {/* Mobile Menu Toggle Button */}
-        <button
-          onClick={toggleMobileMenu}
-          className="lg:hidden flex items-center text-white focus:outline-none transition duration-300 ease-in-out"
-        >
-          <div className="w-6 h-6">
-            <div className="bg-white h-1 w-full mb-1 rounded-full"></div>
-            <div className="bg-white h-1 w-full mb-1 rounded-full"></div>
-            <div className="bg-white h-1 w-full rounded-full"></div>
-          </div>
-        </button>
-      </nav>
-
-      {isMobileMenuOpen && (
-        <div className="lg:hidden mt-2 transition-opacity duration-300 animate-fadeIn">
-          {/* Styled Logo */}
-          <div className="flex items-center justify-center mb-4">
-            {/* Your logo or text here */}
-          </div>
-          <NavLink
-            to="/"
-            onClick={toggleMobileMenu}
-            className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
-            exact
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="skills"
-            onClick={toggleMobileMenu}
-            className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
-          >
-            Skills
-          </NavLink>
-          <NavLink
-            to="projects"
-            onClick={toggleMobileMenu}
-            className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
-          >
-            Projects
-          </NavLink>
-          <NavLink
-            to="certifications"
-            onClick={toggleMobileMenu}
-            className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
-          >
-            Certifications
-          </NavLink>
-          <NavLink
-            to="contact"
-            onClick={toggleMobileMenu}
-            className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
-          >
-            Contact
-          </NavLink>
-          {/* Download CV Button for Mobile */}
-          <a
-            href="src\CV (1).PDF"
-            download="https://c5y18s9mdhikovk1.public.blob.vercel-storage.com/CV%20(1).PDF"
-            className="text-teal-400 block px-4 py-2 font-medium hover:bg-teal-300 hover:text-gray-900 transition duration-300 ease-in-out"
-          >
-            Download CV
-          </a>
-        </div>
-      )}
-
-      <Outlet />
-      <Footer />
-    </div>
-  );
+    );
 };
 
 export default Nav;
